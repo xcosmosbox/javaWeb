ff4745ee879f6bc1a54f7ecc293e40e970732afa
SanCheck <2162381070@qq.com>
2022-12-30 01:07:23 +1100

    using PrintWriter to display web message

--- src/com/javaweb/adapter/LoginServlet.java
+++ src/com/javaweb/adapter/LoginServlet.java
@@ -5,10 +5,13 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class LoginServlet extends GenericServlet{
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
         System.out.println("Login successful!");
+        PrintWriter out = servletResponse.getWriter();
+        out.print("Login successful!");
     }
 }
