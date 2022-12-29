203c3ce0438808e0e8a102e0a703dda45e8dc661
SanCheck <2162381070@qq.com>
2022-12-30 00:42:17 +1100

    comments running times for different functions on wheel lifecycle

--- src/com/javaweb/twodiferservlet/BServlet.java
+++ src/com/javaweb/twodiferservlet/BServlet.java
@@ -7,10 +7,12 @@ import java.io.IOException;
 public class BServlet implements Servlet {
 
     //no-params constructor
+    //only one times for running
     public BServlet(){
         System.out.println("BServlet no-params constructor running!");
     }
 
+    //only one times for running
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
         System.out.println("BServlet init function running!");
@@ -21,6 +23,7 @@ public class BServlet implements Servlet {
         return null;
     }
 
+    //The user calls several times and run several times
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
         System.out.println("BServlet service function running!");
@@ -31,6 +34,7 @@ public class BServlet implements Servlet {
         return null;
     }
 
+    //only one times for release resources
     @Override
     public void destroy() {
         System.out.println("BServlet destroy function running!");
