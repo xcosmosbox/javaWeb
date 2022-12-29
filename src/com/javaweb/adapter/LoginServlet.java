97a1c0d61db4132fbccfcc3605d8ec9706e40b3e
SanCheck <2162381070@qq.com>
2022-12-30 01:25:50 +1100

    using different method to call ServletConfig

--- src/com/javaweb/adapter/LoginServlet.java
+++ src/com/javaweb/adapter/LoginServlet.java
@@ -1,5 +1,6 @@
 package com.javaweb.adapter;
 
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
@@ -8,10 +9,17 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class LoginServlet extends GenericServlet{
+
+    @Override
+    public void init() {
+        System.out.println("Login Servlet's init function running!");
+    }
+
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
         System.out.println("Login successful!");
         PrintWriter out = servletResponse.getWriter();
         out.print("Login successful!");
+        ServletConfig config = this.getServletConfig();
     }
 }
--- src/com/javaweb/adapter/VipLoginServlet.java
+++ src/com/javaweb/adapter/VipLoginServlet.java
@@ -1,5 +1,6 @@
 package com.javaweb.adapter;
 
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
@@ -8,9 +9,17 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class VipLoginServlet extends GenericServlet{
+
+    @Override
+    public void init() {
+        System.out.println("Vip Login Servlet's init function running!");
+    }
+
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
         PrintWriter out = servletResponse.getWriter();
         out.print("Vip login successful!");
+        ServletConfig config = this.getServletConfig();
+
     }
 }
