d166e3d3741a6d000fe7599d9eb6bb48872d28a3
SanCheck <2162381070@qq.com>
2022-12-30 18:14:46 +1100

    Update GetServlet.java

--- src/com/javaweb/httpStudy/GetServlet.java
+++ src/com/javaweb/httpStudy/GetServlet.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class GetServlet extends GenericServlet {
     /**
@@ -25,6 +26,16 @@ public class GetServlet extends GenericServlet {
      */
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
 
+        out.print("<!DOCTYPE html>");
+        out.print("<head>");
+        out.print("<title>from get servlet</title>");
+        out.print("</head>");
+        out.print("<body>");
+        out.print("<h1>from get servlet</h1>");
+        out.print("</body>");
+        out.print("</html>");
     }
 }
