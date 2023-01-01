a6677032bfde91b1da2a1a531d3630e8ef3a111e
SanCheck <2162381070@qq.com>
2023-01-01 20:35:20 +1100

    create template class and template method

--- src/com/designPattern/Person.java
+++ src/com/designPattern/Person.java
@@ -1,4 +1,39 @@
 package com.designPattern;
 
-public class Person {
+public abstract class Person {
+    public final void day(){
+        wakeUp();
+        eatBreakfast();
+        doSome();
+        eatDinner();
+        washing();
+        sleep();
+    }
+
+    private void sleep() {
+        System.out.println("sleep");
+    }
+
+    private void washing() {
+        System.out.println("washing");
+
+    }
+
+    private void eatDinner() {
+        System.out.println("dinner");
+
+    }
+
+    protected abstract void doSome();
+
+    private void eatBreakfast() {
+        System.out.println("breakfast");
+
+    }
+
+    private void wakeUp() {
+        System.out.println("wakeUp");
+
+    }
+
 }
