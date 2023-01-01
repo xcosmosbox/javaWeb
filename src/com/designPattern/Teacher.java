aae2bd843275cbec842d9ee430c284be99fdf1f2
SanCheck <2162381070@qq.com>
2023-01-01 20:36:54 +1100

    Teacher extends Person and implements template method

--- src/com/designPattern/Teacher.java
+++ src/com/designPattern/Teacher.java
@@ -1,4 +1,10 @@
 package com.designPattern;
 
-public class Teacher {
+public class Teacher extends Person {
+
+
+    @Override
+    protected void doSome() {
+        System.out.println("Teaching");
+    }
 }
