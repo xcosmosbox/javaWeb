d69220a75ed43536db4ca1cb59d5560dbf8e52c8
SanCheck <2162381070@qq.com>
2023-01-05 01:31:26 +1100

    Create DeptSaveServlet.java

---
+++ oa_module/src/com/oa/web/action/DeptSaveServlet.java
@@ -0,0 +1,116 @@
+package com.oa.web.action;
+
+import com.oa.utils.DBUtil;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DeptSaveServlet extends HttpServlet {
+    /**
+     * Called by the server (via the <code>service</code> method)
+     * to allow a servlet to handle a POST request.
+     * <p>
+     * The HTTP POST method allows the client to send
+     * data of unlimited length to the Web server a single time
+     * and is useful when posting information such as
+     * credit card numbers.
+     *
+     * <p>When overriding this method, read the request data,
+     * write the response headers, get the response's noBodyWriter or output
+     * stream object, and finally, write the response data. It's best
+     * to include content type and encoding. When using a
+     * <code>PrintWriter</code> object to return the response, set the
+     * content type before accessing the <code>PrintWriter</code> object.
+     *
+     * <p>The servlet container must write the headers before committing the
+     * response, because in HTTP the headers must be sent before the
+     * response body.
+     *
+     * <p>Where possible, set the Content-Length header (with the
+     * {@link ServletResponse#setContentLength} method),
+     * to allow the servlet container to use a persistent connection
+     * to return its response to the client, improving performance.
+     * The content length is automatically set if the entire response fits
+     * inside the response buffer.
+     *
+     * <p>When using HTTP 1.1 chunked encoding (which means that the response
+     * has a Transfer-Encoding header), do not set the Content-Length header.
+     *
+     * <p>This method does not need to be either safe or idempotent.
+     * Operations requested through POST can have side effects for
+     * which the user can be held accountable, for example,
+     * updating stored data or buying items online.
+     *
+     * <p>If the HTTP POST request is incorrectly formatted,
+     * <code>doPost</code> returns an HTTP "Bad Request" message.
+     *
+     * @param request  an {@link HttpServletRequest} object that
+     *             contains the request the client has made
+     *             of the servlet
+     * @param response an {@link HttpServletResponse} object that
+     *             contains the response the servlet sends
+     *             to the client
+     * @throws IOException      if an input or output error is
+     *                          detected when the servlet handles
+     *                          the request
+     * @throws ServletException if the request for the POST
+     *                          could not be handled
+     * @see ServletOutputStream
+     * @see ServletResponse#setContentType
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        request.setCharacterEncoding("UTF-8");
+
+        String deptno = request.getParameter("deptno");
+        String dname = request.getParameter("dname");
+        String loc = request.getParameter("loc");
+
+        // JDBC
+        // Connect database and query all department
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        int count = 0;
+        try {
+            connection = DBUtil.getConnection();
+
+            String sql = "insert into dept(deptno, dname, loc) values(?,?,?)";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,deptno);
+            preparedStatement.setString(2,dname);
+            preparedStatement.setString(3,loc);
+
+            count = preparedStatement.executeUpdate();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.close(connection,preparedStatement,resultSet);
+        }
+
+        if (count == 1){
+            // success
+            // forward servlet
+            request.getRequestDispatcher("/dept/list").forward(request,response);
+        }
+        else {
+            request.getRequestDispatcher("/error.html").forward(request,response);
+        }
+
+    }
+}
