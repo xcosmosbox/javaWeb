6dd004adf8a0d6cbe8dee72b3266acc60373fa55
SanCheck <2162381070@qq.com>
2023-01-04 18:16:18 +1100

    Connect database and query all department for DeptDetailServlet.java

--- oa_module/src/com/oa/web/action/DeptDetailServlet.java
+++ oa_module/src/com/oa/web/action/DeptDetailServlet.java
@@ -1,18 +1,48 @@
 package com.oa.web.action;
 
+import com.oa.utils.DBUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DeptDetailServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
 
         String deptno = request.getParameter("deptno");
+
+        // JDBC
+        // Connect database and query all department
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = DBUtil.getConnection();
+            String sql = "select dname, loc from dept where deptno = ?";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,deptno);
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()){
+                
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.close(connection,preparedStatement,resultSet);
+        }
+
 
 
 
