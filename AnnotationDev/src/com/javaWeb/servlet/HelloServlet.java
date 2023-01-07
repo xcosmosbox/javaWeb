6f1c3a8760b9659828519e96428bc61700487b7d
SanCheck <2162381070@qq.com>
2023-01-07 18:48:09 +1100

    study different annotation tag for servlet

--- AnnotationDev/src/com/javaWeb/servlet/HelloServlet.java
+++ AnnotationDev/src/com/javaWeb/servlet/HelloServlet.java
@@ -5,12 +5,20 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "HelloServlet", value = "/HelloServlet")
+@WebServlet(name = "HelloServlet", value = "/HelloServlet", urlPatterns = {"/hello", "hello2"}, loadOnStartup = 1,
+initParams = {@WebInitParam(name = "username", value = "root"), @WebInitParam(name = "password", value = "123")})
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Getting root path for application
+        String contextPath = request.getContextPath();
 
+        // Set type of response context and type of character set to avoid error encoding
+//        resp.setContentType("text/html;charset=UTF-8");
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
     }
 
     @Override
