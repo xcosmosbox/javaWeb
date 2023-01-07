a68a98a5a03fabf021a05c98a6a298ccb8984d3b
SanCheck <2162381070@qq.com>
2023-01-07 19:09:33 +1100

    iter all 'value' annotation

--- AnnotationDev/src/com/javaWeb/servlet/ReflectAnnotation.java
+++ AnnotationDev/src/com/javaWeb/servlet/ReflectAnnotation.java
@@ -6,13 +6,21 @@ public class ReflectAnnotation {
     public static void main(String[] args) throws Exception {
         Class<?> helloServletClass = Class.forName("com.javaWeb.servlet.HelloServlet");
 
+        // Check it whether have annotation for WebServlet
         boolean helloServletClassAnnotation = helloServletClass.isAnnotationPresent(WebServlet.class);
 
         System.out.println(helloServletClassAnnotation);
 
+        // if yes, getting WebServlet annotation
         if (helloServletClassAnnotation){
             WebServlet webServletAnnotation = helloServletClass.getAnnotation(WebServlet.class);
-            
+
+            // getting WebServlet annotation
+            String[] values = webServletAnnotation.value();
+            for (String value :
+                    values) {
+                System.out.println(value);
+            }
         }
 
     }
