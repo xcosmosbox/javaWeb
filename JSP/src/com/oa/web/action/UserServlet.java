f71f97ca0282002c568fea41207fbb58b9e90112
SanCheck <2162381070@qq.com>
2023-01-08 22:34:27 +1100

    Create UserServlet.java

---
+++ JSP/src/com/oa/web/action/UserServlet.java
@@ -0,0 +1,58 @@
+package com.oa.web.action;
+
+import com.oa.bean.DeptWarpper;
+import com.oa.utils.DBUtil;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@WebServlet(name = "UserServlet", value = "/user/login")
+public class UserServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        boolean success = false;
+
+        // JDBC
+        // Connect database and query all department
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = DBUtil.getConnection();
+
+            String sql = "select * from t_user where username = ? and password = ?";
+
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,username);
+            preparedStatement.setString(2,password);
+
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()){
+                success = true;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.close(connection,preparedStatement,resultSet);
+        }
+
+        if (success){
+            response.sendRedirect(request.getContextPath()+"/dept/list");
+        }else {
+            response.sendRedirect(request.getContextPath()+"/login_error.jsp");
+        }
+
+
+    }
+}
