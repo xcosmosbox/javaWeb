33f723b3bfa3156ed8d5851cd75b013bdc5a18c2
SanCheck <2162381070@qq.com>
2023-01-18 15:08:42 +1100

    Update AccountTransferServlet.java

--- mvc/src/com/javaweb/bank/web/servlet/AccountTransferServlet.java
+++ mvc/src/com/javaweb/bank/web/servlet/AccountTransferServlet.java
@@ -1,10 +1,16 @@
 package com.javaweb.bank.web.servlet;
 
+import com.javaweb.bank.exceptions.MoneyNotEnoughException;
+import com.javaweb.bank.utils.DBUtil;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @WebServlet(name = "AccountTransferServlet", value = "/AccountTransferServlet")
 public class AccountTransferServlet extends HttpServlet {
@@ -18,6 +24,38 @@ public class AccountTransferServlet extends HttpServlet {
         String fromActno = request.getParameter("fromActno");
         String toActno = request.getParameter("toActno");
         double money = Double.parseDouble(request.getParameter("money"));
+
+
+        // Connect database and query all department
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = DBUtil.getConnection();
+            String sql = "select banlance from t_act where actno = ?";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,fromActno);
+            resultSet = preparedStatement.executeQuery();
+
+
+            if (resultSet.next()){
+                double balance = resultSet.getDouble("balance");
+                if(balance < money){
+                    throw new MoneyNotEnoughException("No enough money");
+                }
+
+            }
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.close(connection,preparedStatement,resultSet);
+        }
+
+
 
     }
 }
