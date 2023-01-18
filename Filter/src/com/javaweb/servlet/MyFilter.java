3c160c9eec5623115953469d6d6ff154c837f766
SanCheck <2162381070@qq.com>
2023-01-18 12:55:35 +1100

    Update MyFilter.java

--- Filter/src/com/javaweb/servlet/MyFilter.java
+++ Filter/src/com/javaweb/servlet/MyFilter.java
@@ -5,16 +5,20 @@ import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 
-@WebFilter(filterName = "MyFilter")
+//@WebFilter(filterName = "MyFilter", value = {"/a.do","/b.do"})
+@WebFilter(filterName = "MyFilter", value = {"*.do"})
 public class MyFilter implements Filter {
     public void init(FilterConfig config) throws ServletException {
+        System.out.println("do filter init function");
     }
 
     public void destroy() {
+        System.out.println("do filter destroy function");
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        System.out.println("do filter doFilter function");
         chain.doFilter(request, response);
     }
 }
