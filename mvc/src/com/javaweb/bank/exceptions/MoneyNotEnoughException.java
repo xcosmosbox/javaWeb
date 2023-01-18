d9ae4f6da1768bc806474b32d0485139dff6bc17
SanCheck <2162381070@qq.com>
2023-01-18 15:08:38 +1100

    Create MoneyNotEnoughException.java

---
+++ mvc/src/com/javaweb/bank/exceptions/MoneyNotEnoughException.java
@@ -0,0 +1,11 @@
+package com.javaweb.bank.exceptions;
+
+public class MoneyNotEnoughException extends Exception{
+    public MoneyNotEnoughException(){
+
+    }
+
+    public MoneyNotEnoughException(String msg){
+        super(msg);
+    }
+}
